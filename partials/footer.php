15fd5a698fe818aa9f5d4091c4d45cfb9b4d2e94
Staish-khalid <118855150+Staish-khalid@users.noreply.github.com>
2024-01-06 14:49:33 -0800

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,42 @@
+    <footer>
+        <div class="footer-content">
+            <ul>
+            	<li><h4>Categories</h4></li>
+                <?php  
+                    require_once 'config/dbc.php';
+                    $getAllCategories = mysqli_query($connection, "SELECT * FROM category WHERE status='ACTIVE' ORDER BY rand() LIMIT 5") or die(mysqli_error($connection));
+                    while ($viewAllCategories = mysqli_fetch_array($getAllCategories)) {
+                ?>
+                <li><a href="category.php?id=<?php echo $viewAllCategories['id']; ?>"><?php echo $viewAllCategories['title']; ?></a></li>
+                <?php } ?>
+            </ul>
+            
+            <ul>
+            	<li><h4>Latest SMS</h4></li>
+                <?php  
+                    require_once 'config/dbc.php';
+                    $getLatestSMS = mysqli_query($connection, "SELECT * FROM message WHERE status='ACTIVE' ORDER BY id DESC LIMIT 5") or die(mysqli_error($connection));
+                    while ($viewLatestSMS = mysqli_fetch_array($getLatestSMS)) {
+                ?>
+                <li><a href="sms.php?id=<?php echo $viewLatestSMS['id']; ?>"><?php echo $viewLatestSMS['title']; ?></a></li>
+                <?php } ?>
+            </ul>
+            
+            <ul class="endfooter">
+            	<li><h4>Follow Us</h4></li>
+                <li><a href="https://www.facebook.com/AlFateemAcademyOfficial/" target="_blank">Facebook </a></li>
+                <li><a href="#" target="_blank">Twitter</a></li>
+                <li><a href="#" target="_blank">Linked In</a></li>
+                <li><a href="#" target="_blank">Pinterest</a></li>
+                <li><a href="#" target="_blank">YouTube</a></li>
+            </ul>
+            
+            <div class="clear"></div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; YourSite 2022. by Students of AFA</p>
+         </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home | AFA Bulk SMS</title>
+<link rel="stylesheet" href="assets/css/styles.css" type="text/css" />
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+<div id="container">
+    <header>
+    	<h1><a href="/">AFA<span>BULK SMS</span></a></h1>
+        <h2>Laugh, giggle, gaffaws</h2>
+    </header>
+    <nav>
+    	<ul>
+        	<li class="start selected"><a href="index.html">Home</a></li>
+            <li class=""><a href="examples.html">About Us</a></li>
+            <li><a href="#">Our Vision</a></li>
+            <li><a href="#">Our Mission</a></li>
+            <li class="end"><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+	<img src="assets/images/image.jpg" alt="Buildings" />
+
